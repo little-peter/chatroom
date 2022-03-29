3e83d89fa597f990b9925e3e01718e187f0247b1
little-peter <73474126+little-peter@users.noreply.github.com>
2022-03-29 15:33:08 +0800

    Update TestWebSocket.java

--- web聊天室2.0/src/main/java/org/example/test/TestWebSocket.java
+++ web聊天室2.0/src/main/java/org/example/test/TestWebSocket.java
@@ -5,14 +5,7 @@ import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * 文档注释 一般用于 方法的注释和类的注释
- * User: xu Yang
- * Date: 2021-12-2021/12/26
- * Time: 16:55
- */
+
 
 @ServerEndpoint(value="/test/{userId}")
 public class TestWebSocket {
